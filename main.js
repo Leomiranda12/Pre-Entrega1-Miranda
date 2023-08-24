34215125a092fe9b37aaa95974dc1918de1ca73b
Leomiranda12 <130712855+Leomiranda12@users.noreply.github.com>
2023-08-23 22:33:34 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,100 @@
+const shopContent = document.getElementById("shopContent")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("modal-container")
 const productos = [
-    { nombre : "arroz", precio : 100 },
-    { nombre : "galletas", precio : 150 },
-    { nombre : "leche", precio : 200 },
-    { nombre : "gaseosa", precio : 250 },
-    { nombre : "vino", precio : 350 },
-] ;
-let carrito = []
-let seleccion = prompt("¿Hola desea comprar algun producto?")
-while (seleccion != "si" && seleccion != "no") {
-    alert("Por favor ingrese si o no")
-    seleccion = prompt("¿Hola desea comprar algun producto?")
-}
+    {
+        id: 1,
+        nombre: "Harina",
+        precio: 50,
+        img:"https://www.shutterstock.com/es/image-photo/alameda-ca-july-21-2021-five-2012091740"
+    },
+    {
+        id: 2,
+        nombre: "masitas",
+        precio: 150,
+        img: "https://www.shutterstock.com/es/image-photo/oatmeal-cookies-package-isolated-on-white-2115512879"
 
-if(seleccion == "si"){
-    alert("Aqui estan los productos")
-    let todoslosProductos = productos.map(
-        (producto) => producto.nombre + " " + producto.precio + "$"
-    );
-    alert(todoslosProductos.join(" -- "))
-} else if (seleccion == "no"){
-    alert("Gracias por venir,nos vemos pronto")
-}
+    },
+    {
+        id: 3,
+        nombre: "leche",
+        precio: 200,
+        img: "https://www.shutterstock.com/es/image-photo/fresh-milk-beverage-carton-107951936"
+    },
+    {
+        id: 4,
+        nombre: "vino",
+        precio: 400,
+        img: "https://www.shutterstock.com/es/image-photo/bottle-wine-pothography-classy-style-mock-2005859909"
+    },
+];
+let carrito = [];
+productos.forEach((product)=> {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="price">${product.precio}$</p>
+    `;
 
-while (seleccion != "no") {
-    let producto = prompt("agrega uno o mas productos a tu carrito")
-    let = precio = 0
-    if (producto == "arroz" || producto == "galletas" || producto == "leche" || producto == "gaseosa" || producto == "vino"){
-        switch (producto) {
-            case "arroz":
-                precio = 100;               
-                break;
-            case "galletas":
-                precio = 150;               
-                break;
-            case "leche":
-                precio = 200;               
-                break;
-            case "gaseosa":
-                precio = 250;               
-                break;
-            case "vino":
-                precio = 350;               
-                break;
-            default:
-                break;
-        }
-    let unidades = parseInt(prompt("Cuantas unidades desea llevar"))
-    carrito.push({producto, unidades, precio})    
-    } else {
-        alert("No tenemos ese producto")
-    }
+    shopContent.append(content)
 
-    seleccion = prompt("¿Desea seguir comprando?")
-    
-    while (seleccion === "no") {
-        alert("Aqui esta la lista de su carrito")
-        carrito.forEach((carritoFinal) => {
-            alert(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}` )
-        })
-        break;
-    }
-}
+    let comprar = document.createElement("button")
+    comprar.innerText = "comprar";
+    comprar.className = "comprar"
 
-const total = carrito.reduce((ecc, el) => ecc + el.precio * el.unidades, 0)
-alert(`el total a pagar por su compra es: ${total}`)
+    content.append(comprar);
+    comprar.addEventListener("click", () =>{
+        carrito.push({
+            id:product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio,
+        });
+        console.log(carrito);
+    });
+});
+
+verCarrito.addEventListener("click", () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex"
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header"
+    modalHeader.innerHTML = `
+    <h1 class="modal-header-title">Carrito.</h1>
+    `;
+
+    modalContainer.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+    modalHeader.append(modalbutton);
+
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div");
+        carrito.className = "modal-content";
+        carrito,innerHTML = `
+        <img src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p>${product.precio}$</p>
+        `;
+
+        modalContainer.append(carritoContent);
+    });
+
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar: ${total} $`
+    modalContainer.append(totalBuying);
+
+});
+
